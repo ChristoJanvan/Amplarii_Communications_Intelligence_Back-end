61768d90a5a4502e46d5fa1d6aa6fb73e2b7018a
ChristoJanvan <christo@technevest.com>
2025-08-03 13:16:26 +0200

    Remove conflicting files

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: '*', // Allow all origins for now
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
